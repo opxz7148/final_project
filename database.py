f0ee4779b23c62808a7ba4833cc92238c59f077e
Pasu Sangiemsin <opxz7148@gmail.com>
2023-11-21 22:14:19 +0700

    Implement database and table class

--- database.py
+++ database.py
@@ -1,19 +1,145 @@
 # try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
 
 import csv, os
+import copy
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
 
 # add in code for a Database class
+class Database:
+    # Construct database class by setting database name an create db list to contain table in the future
+    def __init__(self, name):
+        """
+        Construct database class by setting database name an create db list to contain table in the future
+        :param name: Name of the database
+        """
+        self.name = name
+        self.db = []
+
+    # Method to insert new data table into database
+    def insert_table(self, table):
+        """
+        function to insert new data table into database.
+        :param table: Table object
+        """
+        if isinstance(table, Table):
+            self.db = dict
+        else:
+            raise ValueError("Table to insert in to be instance of Table class")
+
+    def search(self, table_name):
+        """
+        funtion to find certain table in database
+        :param table_name: name of table that user want to search.
+        :return: Table instance or none
+        """
+        for table in self.db:
+            if table.table_name == table_name:
+                return table
+        return None
+
+    def add_csv_table(self, filename):
+        """
+        Import new dataset from csv file and add to database
+        :param filename: Name of csv file that user want to add to database
+        :return:
+        """
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+        lst = []
+        with open(os.path.join(__location__, filename)) as f:
+            rows = csv.DictReader(f)
+            for r in rows:
+                lst.append(dict(r))
+        self.db.append(Table(filename, lst))
+
 
 # add in code for a Table class
+class Table:
+    def __init__(self, name, dict_lst):
+        """
+        Construct a table class to contain data in list of dict format
+        :param name: String name of table
+        :param dict_lst: List of dictionaries
+        """
+        self.table_name = name
+        self.table = dict_lst
+
+    def insert(self, info):
+        """
+        Use to insert new set of information into a table as dict format.
+        :param info: dictionaries of information
+        """
+        if isinstance(info, dict):
+            self.table.append(info)
+
+    def __floatable(self, element):
+        """
+        Internal function to check is certain information are able to convert to float or not.
+        :param element: Element that user want to check
+        """
+        if element is None:
+            return False
+
+        try:
+            float(element)
+            return True
+
+        except ValueError:
+            return False
+
+    def join(self, other_table, common_key):
+        """
+        Function to join other table then return a new table that combine both table with common key that user want to filter.
+        :param other_table: Other Table instance
+        :param common_key: Key that user want to include in new table
+        :return:
+        """
+        joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+
+        return joined_table
+
+    def filter(self, condition):
+        """
+        Function to filter table with certain condition
+        :param condition: Condition that user want to filter as a lambda function
+        :return: New table instance that already filtered.
+        """
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+
+    def aggregate(self, function, aggregation_key):
+        """
+        Function to gather certain information and do some calculation with it
+        :param function: Function that user want to do with list of information.
+        :param aggregation_key: Key of information that user want to work with.
+        :return: Result of function
+        """
+        temps = []
+        for item1 in self.table:
+            if self.__floatable(item1[aggregation_key]):
+                temps.append(float(item1[aggregation_key]))
+            else:
+                temps.append(item1[aggregation_key])
+        return function(temps)
+
 
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
+mydb = Database("db1")
+mydb.add_csv_table("persons.csv")
+person_table = mydb.search("persons.csv")
+print(*person_table.table, sep="\n")
+print()
+person_table.insert({'ID': '111111', 'fist': 'Ohm', 'last': 'S.', 'type': 'student'})
+print(*person_table.table, sep="\n")
+
