07ae7d1ac1afb1aa6f1b9b4678ee76d620b4652f
Pasu Sangiemsin <opxz7148@gmail.com>
2023-12-13 10:55:26 +0700

    Implement print and get choice to make it easier to input a choice from a user

--- helper.py
+++ helper.py
@@ -12,7 +12,7 @@ def login(db):
         if username == user["username"]:
             if user["password"] == password:
                 # returns [ID, role] if valid
-                return [user["ID"], user["role"]]
+                return [user["username"], user["ID"], user["role"]]
             else:
                 # otherwise returning None
                 print("Password incorrect")
@@ -35,8 +35,8 @@ def write_csv(filename, listofhead, listofdict):
     file.close()
 
 
-def get_int(msg):
-
+def get_int(msg, start=0, stop=10000):
+    in_range = list(range(start, stop+1))
     while True:
         result = input(msg)
         try:
@@ -44,9 +44,32 @@ def get_int(msg):
         except ValueError:
             continue
 
+        if result not in in_range:
+            continue
         return result
 
 
 def get_str(msg):
-    return input(msg)
+
+    while True:
+        result = input(msg)
+        try:
+            result = int(result)
+        except ValueError:
+            if result.strip() == "":
+                continue
+            return result
+
+        continue
+
+
+def print_get_choice(choice_lst):
+
+    print("")
+    for choice in range(len(choice_lst)):
+        print(f"    [{choice + 1}] {choice_lst[choice]}")
+    print("")
+
+    ans = get_int("Select your action: ", 1, len(choice_lst))
+    return ans
 
